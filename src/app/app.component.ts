b7829386423b1e85523a567a6cde5685fe84901e
yakupcelikk <yakuupcelik@gmail.com>
2021-09-06 15:07:53 +0300

    Initial commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { lorem } from "faker";
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+
+  onInput(e: any) {
+    e.currentTarget.value;
+    console.log(e.currentTarget.value);
+  }
 }
+
